db62753c507613219c0a3434c7c10eb41570d77f
Hidaya <69722872+heedoooch@users.noreply.github.com>
2021-05-28 17:13:10 +0100

    Update Offres.js

--- screens/Offres.js
+++ screens/Offres.js
@@ -1,51 +1,78 @@
-import React, {Component} from 'react';
-import {StyleSheet, ScrollView, View, Text, Image} from 'react-native';
+import React ,{ useState, useEffect } from 'react';
+import { ActivityIndicator, FlatList, View, Text , StyleSheet } from 'react-native';
+import firebase from 'firebase';
 
+const Alert = () => {
+  //
+  const [loading, setLoading] = useState(true); // Set loading to true on component mount
+  const [alerts, setalerts] = useState([]); // Initial empty array of alerts
 
-export default function offers({navigation}) {
-
-    return(
-    <ScrollView>
-      <View style = {styles.itemRow}>
-         <Image source ={require('../assets/card.png')} style ={styles.itemImage} />
-         <Text style={styles.itemText}>Abonnement TAWASSOL CLASSIC</Text>
-         <Text style={styles.itemText}>1000DA/mois</Text>
-
-         <Image source ={require('../assets/kids.jpg')} style ={styles.itemImage} />
-         <Text style={styles.itemText}>Abonnement TAWASSOL JUNIOR</Text>
-         <Text style={styles.itemText}>840DA/mois</Text>
-
-         <Image source ={require('../assets/old.jpg')} style ={styles.itemImage} />
-         <Text style={styles.itemText}>Abonnement TAWASSOL SENIOR</Text>
-         <Text style={styles.itemText}>720DA/mois</Text>
-
-         <Image source ={require('../assets/college.jpg')} style ={styles.itemImage} />
-         <Text style={styles.itemText}>Abonnement TAWASSOL DJAMI3I</Text>
-         <Text style={styles.itemText}>600DA/mois</Text>
-      </View>
-    
-    </ScrollView>
-      
-    )
+  useEffect(() => {
+    const subscriber = firebase.firestore()
+      .collection('alerts')
+      .onSnapshot(querySnapshot => {
+        const alerts = [];
+  
+        querySnapshot.forEach(documentSnapshot => {
+          alerts.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+  
+        setalerts(alerts);
+        setLoading(false);
+      });
+  
+    // Unsubscribe from events when no longer in use
+    return () => subscriber();
+  }, []);
+  if (loading) {
+    return <ActivityIndicator />;
   }
 
- const styles = StyleSheet.create({
-   container: {
-     marginTop: 20,
-     backgroundColor: '#f5fcff',
-   },
-   itemRow:{
-     borderBottomColor: '#ccc',
-     marginBottom:10,
-     borderBottomWidth: 1
-   },
-   itemText:{
-     fontSize: 16,
-     padding: 5
-   },
-   itemImage:{
-     width:'100%',
-     height:200,
-     resizeMode:'cover',
-   }
- })
+  return (
+    <View style={styles.container}>
+      <FlatList
+      data={alerts}
+      renderItem={({ item , index }) => (
+        <View style={styles.item }>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.report}>{item.report}</Text>
+          <Text style={styles.date}>{item.date + " " + item.time}</Text>
+        </View>
+      )}
+    />
+    </View>
+  );
+}
+export default Alert;
+
+const styles = StyleSheet.create({  
+  container: {  
+      flex: 1,  
+  },  
+  item: {  
+    borderColor : '#0DB0A8',
+    borderStyle : 'solid' ,
+    borderWidth : 5,
+    borderRadius : 13 ,
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,  
+    backgroundColor : 'white'
+  },  
+  title : {
+    color : '#0DB0A8',
+    fontSize : 20,
+    fontWeight:"bold",
+    marginBottom : 8
+  },
+  report : {
+    color : '#0DB0A8',
+    fontSize : 18,
+  },
+  date : {
+    textAlign : "right"
+  },
+})
